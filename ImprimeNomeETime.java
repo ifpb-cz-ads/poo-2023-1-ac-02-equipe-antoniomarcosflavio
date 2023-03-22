e472ecd4ff30046eba44d690d2a180e37d1f2a59
Antonio Marcos <117120474+AntonioMarcosDev@users.noreply.github.com>
2023-03-22 20:11:03 -0300

    Criando a Classe ImprimeNomeETime.java

---
+++ ImprimeNomeETime.java
@@ -0,0 +1,35 @@
+//Questão 06
+public class ImprimeNomeETime {
+    public static void main(String[] args) {
+        String nome = "Antonio Marcos";
+        String time = "Fluminense";
+        System.out.println(nome);
+        System.out.println(time);
+    }
+}
+/*
+Questão 07
+PUBLIC CLASS IMPRIMENOMEETIME {
+    PUBLIC STATIC VOID MAIN(STRING[] ARGS) {
+        STRING NOME = "ANTONIO MARCOS";
+        STRING TIME = "FLUMINENSE";
+        SYSTEM.OUT.PRINTLN(NOME);
+        SYSTEM.OUT.PRINTLN(TIME);
+    }
+}
+Houve erro de sintáxe no compilador Java, porque ele não foi capaz de reconhecer os nomes das classes e métodos.
+*/
+
+/*
+Questão 08
+public class Time {
+    public static void main(String[] args) {
+        String nome = "Antonio Marcos";
+        String time = "Fluminense";
+        System.out.println(nome);
+        System.out.println(time);
+    }
+}
+O código não chegou a ser compilado, uma vez que a classe é renomeada para Time, o compilador informa que ela deve ser declarada num arquivo de mesmo nome, no caso
+em tela, o arquivo teria de ser renomeado para o mesmo nome da classe: Time.java.
+*/
